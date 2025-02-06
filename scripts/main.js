bcdf7146af5e7644288951e13723ddec7c06da9c
Sauryah <sahilpradhanchess@gmail.com>
2025-02-06 12:45:58 +0530

    style.css and main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,5 @@
-
+// Contact Form Submission (Just for demonstration)
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Form submitted!');
+});
